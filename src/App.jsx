d12128f069bdceaa765ff1d03b9b35b4d1b8357f
Wakil-Mahmud <atfirst42@gmail.com>
2025-09-27 10:00:29 +0600

    added the navbar, main, and the footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from './components/navbar/Navbar';
 import Main from './components/main/Main'
 import Footer from './components/footer/Footer';
+import Task from './components/Task/Task';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
     <Navbar></Navbar>
     <Main></Main>
 
-    
+    <Task></Task>
 
     <Footer></Footer>
 
